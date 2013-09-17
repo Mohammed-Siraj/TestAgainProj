1b6f776fd94b0cbad044234ed41fe1927acfa11d
mahmed4 <mahmed4@mahmed4-ws.partnet.cisco.com>
2013-09-17 13:15:54 +0530

    This is the project addition

---
+++ src/test/ForXML.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ForXML {
+	
+@Before
+public void testBefore() {
+	System.out.print("\n" + "This is before class");
+		
+ }
+
+@Test
+public void testTest() {
+	System.out.print("\n" + "This is before class");
+		
+ }
+
+@After
+public void testAfter() {
+	System.out.print("\n" + "This is before class");
+		
+ }
+
+}
